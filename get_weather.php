9e15fa8acb9072ea267f1f14e29da7f66fddbd15
simondockal <101214260+simondockal@users.noreply.github.com>
2024-03-12 13:14:48 +0100

    fungujesnad

--- get_weather.php
+++ get_weather.php
@@ -1,49 +1,60 @@
 <?php
+if (isset($_POST['mesto']) && isset($_POST['od']) && isset($_POST['do'])) {
+    $city = strtolower($_POST['mesto']);
+    $od = strtotime($_POST['od']);
+    $do = strtotime($_POST['do']);
 
-if (isset($_POST['city'])) {
-    $city = strtolower($_POST['city']);
-    
-    // Definujte informace pro jednotlivá města
     $cities = array(
-        'trebic' => array('lat' => 50.073658, 'lon' => 13.818540),
-        'jihlava' => array('lat' => 49.396548, 'lon' => 15.590650),
-        'zdar' => array('lat' => 49.562473, 'lon' => 15.939089),
-        'pelhrimov' => array('lat' => 49.431576, 'lon' => 15.583588)
+        'praha' => array('lat' => 50.073658, 'lon' => 13.818540),
+        'brno' => array('lat' => 49.396548, 'lon' => 15.590650),
+        'ostrava' => array('lat' => 49.562473, 'lon' => 15.939089),
+        'plzen' => array('lat' => 49.431576, 'lon' => 15.583588),
+        'liberec' => array('lat' => 50.769479, 'lon' => 15.082995),
+        'ceske-budejovice' => array('lat' => 48.974468, 'lon' => 14.474622)
     );
 
-    // Získání informací o počasí pro vybrané město
+        // Zkontroluje, zda je zadané město v poli $cities
     if (array_key_exists($city, $cities)) {
-        $lat = $cities[$city]['lat'];
-        $lon = $cities[$city]['lon'];
+        // Město je v poli, uložíme ho do proměnné $selected_city
+        $selected_city = $city;
+        // Uložíme data města do proměnné $city_data
+        $weatherData = $cities[$selected_city];
+    } else {
+        // Město nenalezeno v poli, vypíšeme alert
+        echo '<script>alert("Město nenalezeno.");</script>';
+    }
 
-        $url = "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=4921810ad61b7cc86d5d721dbdee166f";
+
+    while ($od <= $do) {
+        $date = date('Y-m-d', $od);
+        $url = "https://api.openweathermap.org/data/2.5/weather?lat={$cities[$city]['lat']}&lon={$cities[$city]['lon']}&appid=4921810ad61b7cc86d5d721dbdee166f";
 
         $urlContents = file_get_contents($url);
-
         $weatherArray = json_decode($urlContents, true);
 
         if ($weatherArray) {
+            $temperature = ((int)$weatherArray['main']['temp'] - 273);
             $humidity = $weatherArray['main']['humidity'];
             $sunset = date('H:i:s', $weatherArray['sys']['sunset']);
-            $temperature = ((int)$weatherArray['main']['temp'] - 273);
+            $description = $weatherArray['weather'][0]['description'];
 
-            $response = array(
+            $weatherData[] = array(
+                'date' => $date,
                 'city' => $weatherArray['name'],
                 'temperature' => $temperature,
-                'description' => $weatherArray['weather'][0]['description'],
                 'humidity' => $humidity,
-                'sunset' => $sunset
+                'sunset' => $sunset,
+                'description' => $description
             );
-
-            echo json_encode($response);
         } else {
             // Handle case where weather information is not available
             echo json_encode(array('error' => 'Není k dispozici informace pro toto město.'));
         }
-    } else {
-        // Handle case where city information is not available
-        echo json_encode(array('error' => 'Není k dispozici informace pro toto město.'));
-    }
-}
 
+        // Přidání jednoho dne k $od
+        $od = strtotime('+1 day', $od);
+    }
+
+    echo json_encode($weatherData);
+}
 ?>
